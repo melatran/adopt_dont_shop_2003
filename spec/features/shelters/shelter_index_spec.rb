88ba7f0cc3aa95d60bd9d3054ec646490002b2b4
Melanie Tran <melaniexx02@gmail.com>
2020-05-05 15:27:33 -0600

    Update name of rspec test for shelter to include more than one test

--- spec/features/shelters/shelter_index_spec.rb
+++ spec/features/shelters/shelter_index_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe "as a user, when i visit the shelters index", type: :feature do
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
   end
+
+  it "can see the attributes of the shelter with that id"
+  shelter_1 = Shelter.create(
+    name: "Paws For You"
+    address: "1234 W Elf Ave"
+    city: "Denver"
+    state: "Colorado"
+    zip: "90210")
+  shelter_2 = Shelter.create(name: "Furry Pals")
+
 end
