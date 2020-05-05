a5285a3dacef10f0c446edcdc2e09589995931aa
Melanie Tran <melaniexx02@gmail.com>
2020-05-05 15:37:45 -0600

    Update with commas added

--- spec/features/shelters/shelter_index_spec.rb
+++ spec/features/shelters/shelter_index_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe "as a user, when i visit the shelters index", type: :feature do
   it "can see the names of each shelter" do
     shelter_1 = Shelter.create(
-      name: "Paws For You"
-      address: "1234 W Elf Ave"
-      city: "Denver"
-      state: "Colorado"
+      name: "Paws For You",
+      address: "1234 W Elf Ave",
+      city: "Denver",
+      state: "Colorado",
       zip: "90210")
 
     shelter_2 = Shelter.create(
-      name: "Forever Pet"
-      address: "1234 S Ahgase Blvd"
-      city: "Boulder"
-      state: "Colorado"
+      name: "Forever Pet",
+      address: "1234 S Ahgase Blvd",
+      city: "Boulder",
+      state: "Colorado",
       zip: "90210")
 
     visit "/shelters"
