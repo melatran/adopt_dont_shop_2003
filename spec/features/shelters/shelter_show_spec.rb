d4c67449c3e03676ff42dafdac10531bfe55cfe6
Melanie Tran <melaniexx02@gmail.com>
2020-05-05 15:35:42 -0600

    Created show spec test for shelters

---
+++ spec/features/shelters/shelter_show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "as a user, when i visit the shelters index", type: :feature do
+  it "can see the attributes of the shelter with that id"
+  shelter_1 = Shelter.create(
+    name: "Paws For You"
+    address: "1234 W Elf Ave"
+    city: "Denver"
+    state: "Colorado"
+    zip: "90210")
+
+  shelter_2 = Shelter.create(
+    name: "Forever Pet"
+    address: "1234 S Ahgase Blvd"
+    city: "Boulder"
+    state: "Colorado"
+    zip: "90210")
+
+  visit "/shelters/:id"
+  expect(page).to have_content(shelter_1.name)
