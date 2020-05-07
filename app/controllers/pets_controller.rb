df990467de8a05b10acd7dc3f2e0f24b324cd58e
Melanie Tran <melaniexx02@gmail.com>
2020-05-06 19:35:55 -0600

    Added rspec test for shelter pet index

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,8 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def list_shelter_pets_index
+    @shelter = Shelter.find(params[:shelter_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+  get '/shelters/:id/pets', to: 'pets#list_shelter_pets_index'
 end
