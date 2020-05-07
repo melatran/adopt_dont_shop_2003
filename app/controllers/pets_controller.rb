0744b09bc87250b17a9ee870899a0b709826cdfd
Melanie Tran <melaniexx02@gmail.com>
2020-05-07 15:33:01 -0600

    Create pets option for users

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,21 +7,28 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
+  def new
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    shelter = Shelter.find(params[:id])
+    shelter.pets.create(pet_params)
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
+
   def edit
     @pet = Pet.find(params[:id])
   end
 
   def update
-    pet = Pet.find(params[:id])
-    pet.update({
-      image: params[:pet][:image],
-      name: params[:pet][:name],
-      description: params[:pet][:description],
-      approximate_age: params[:pet][:approximate_age],
-      sex: params[:pet][:sex]
-      })
-
+    pet = Pet.update(pet_params)
     pet.save
     redirect_to '/pets/#{pet.id}'
+  end
+
+  private
+  def pet_params
+    params.permit(:image, :name, :description, :sex)
   end
 end
